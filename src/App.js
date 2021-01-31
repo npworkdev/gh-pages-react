90a53de50d4bad7f2c7a1c597a31e2b347570c38
NP <np@workmail.com>
2021-01-30 20:50:22 -0500

    Changed landing page for github repository

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          NPWorkDev <code>React page</code> of GitHub Repository
         </p>
         <a
           className="App-link"
